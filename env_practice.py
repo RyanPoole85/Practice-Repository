ce188a8834fe4d89e66517ab0eb5baaff25dd4a3
Ryan Poole <ryan.anthony.poole@gmail.com>
2023-12-21 11:52:43 -0600

    Update env_practice.py

--- env_practice.py
+++ env_practice.py
@@ -1,8 +1,6 @@
-host = 'data.codeup.com'
-user = 'ursula_2328'
-password = 'ZEsOi3USk9qClmhKK80EWvX6pZaO4rTk'
-#def get_db_url(user, password, host, database):
-#   return f'mysql+pymysql://ursula_2328:ZEsOi3USk9qClmhKK80EWvX6pZaO4rTk@data.codeup.com/employees'
+host = ''
+user = ''
+password = ''
+#def get_db_url(user, password, host, 
 def get_db_url(db, user=user, password=password, host=host):
-    return (f'mysql+pymysql://{user}:{password}@{host}/{db}')
-#url = f'mysql+pymysql://ursula_2328:ZEsOi3USk9qClmhKK80EWvX6pZaO4rTk@data.codeup.com/employees'
+    return (f'mysql+pymysql://{user}:{password}@{host}/{
