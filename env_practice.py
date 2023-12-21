6fe37942d1b547750959386da3cc600953ca892a
Ryan Poole <ryan.anthony.poole@gmail.com>
2023-12-21 15:29:51 -0600

    Delete env_practice.py

--- env_practice.py
+++
@@ -1,6 +0,0 @@
-host = ''
-user = ''
-password = ''
-#def get_db_url(user, password, host, 
-def get_db_url(db, user=user, password=password, host=host):
-    return (f'mysql+pymysql://{user}:{password}@{host}/{
